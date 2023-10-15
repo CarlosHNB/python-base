96d529ab949022fd82a4b4b7bbaae6508a09cbc9
CarlosHNB <carloshnb.um@gmail.com>
2023-10-15 18:26:48 -0300

    Programa tabuada

---
+++ tabuada.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""Imprimi a tabuada do 1 ao 10.
+
+---Tabuada do 1---
+    1 x 1 = 1
+...
+##################
+---Tabuada do 2---
+    2 x 1 = 2
+...
+##################
+"""
+
+__version__ = "0.1.1"
+__author__ = "Carlos"
+
+# numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numeros = list(range(1, 11))
+
+# Iterable (percorriveis)
+
+# para cada numero em numeros:
+for n1 in numeros:
+    print("{:-^18}".format(f"Tabuada do {n1}"))
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}\n"))
+    print("#"*18)
