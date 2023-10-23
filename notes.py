79c97ffb3c4e33a713bea4c065d3c57bd1dffcba
CarlosHNB <carloshnb.um@gmail.com>
2023-10-23 16:10:03 -0300

    Implementando o loop while para que o usuário possa decidir se quer adicionar/ler notas

--- notes.py
+++ notes.py
@@ -33,29 +33,41 @@ if not arguments:
 if arguments[0] not in cmds:
     print(f"Invalid command {arguments[0]}")
     
+while True:
     
-if arguments[0] == "read":
-    # Leitura das notas
-    
-    for line in open(filepath):
+    if arguments[0] == "read":
+        try:
+            arg_tag = arguments[1].lower()
+        except IndexError:
+            arg_tag = input("Qual a tag:").strip().lower()
         
-        title, tag, text = line.split("\t")
-        if tag.lower() == arguments[1].lower():
-            print(f"title: {title}")
-            print(f"text: {text}")
-            print("-" * 30)
-            print()
-            
-if arguments[0] == "new":
-    title = arguments[1] # TODO: trocar exceptions
-    text = [
-        f"{title}",
-        input("tag:").strip(),
-        input("text:\n").strip(),
-    ]
-       
-       # \t tsv - separar com tab
+        # Leitura das notas  
+        for line in open(filepath):
+            title, tag, text = line.split("\t")
+            if tag.lower() == arg_tag:
+                print(f"title: {title}")
+                print(f"text: {text}")
+                print("-" * 30)
+                print()
+                
+    if arguments[0] == "new":
+        try:
+            title = arguments[1] 
+        except IndexError:
+            title = input("Qual é o titulo:").strip().title()
+        
+        text = [
+            f"{title}",
+            input("tag:").strip(),
+            input("text:\n").strip(),
+        ]
+        
+        # \t tsv - separar com tab
+        
+        with open(filepath, "a") as file_:
+            file_.write("\t".join(text) + "\n")
     
-    with open(filepath, "a") as file_:
-        file_.write("\t".join(text) + "\n")
+    continuar = input(f"Quer continuar {arguments[0]} notas? [N/y]").strip().lower()
+    if continuar != "y":
+        break
 
