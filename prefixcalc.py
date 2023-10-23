7ec9b0bc8dbfd846cdf70e5888f8b549857d1c0c
CarlosHNB <carloshnb.um@gmail.com>
2023-10-23 16:23:20 -0300

    Implementando a opção de sair ou fazer novas operações utilizando o 'While'

--- prefixcalc.py
+++ prefixcalc.py
@@ -34,70 +34,75 @@ import os
 import sys
 from datetime import datetime
 
-arguments = sys.argv[1:]
 
-# Validação
-if not arguments:
-    operation = input("Operação:")
-    n1 = input("n1:")
-    n2 = input("n2:")
-    arguments = [operation, n1, n2]
+while True:
+
+    arguments = sys.argv[1:]
+
+    # Validação
+    if not arguments:
+        operation = input("Operação:")
+        n1 = input("n1:")
+        n2 = input("n2:")
+        arguments = [operation, n1, n2]
+        
+    elif len(arguments) != 3: 
+        print("Número de argumentos inválidos")
+        print("ex: `sum 5 2`")
+        sys.exit(1)
+
+    operation, *nums = arguments
+
+
+    valid_operations = ("sum", "sub", "mult", "div")
+
+    if operation not in valid_operations:
+        print("Operação inválida")
+        print(valid_operations)
+        sys.exit(1)
+
+    validated_nums = []
+    for num in nums:
+        # TODO: Repetição while + exceptions
+        if not num.replace(".", "").isdigit():
+            print(f"Número inválido {num}")
+            sys.exit()
+        if "." in num:
+            num = float(num)
+        else:
+            num = int(num)
+        validated_nums.append(num)
     
-elif len(arguments) != 3: 
-    print("Número de argumentos inválidos")
-    print("ex: `sum 5 2`")
-    sys.exit(1)
 
-operation, *nums = arguments
+    n1, n2 = validated_nums
 
 
-valid_operations = ("sum", "sub", "mult", "div")
+    # TODO: Usar dict de funções
+    if operation == "sum":
+        result = n1 + n2
+    elif operation == "sub":
+        result = n1 - n2
+    elif operation == "mult":
+        result = n1 * n2
+    elif operation == "div":
+        result = n1 / n2
 
-if operation not in valid_operations:
-    print("Operação inválida")
-    print(valid_operations)
-    sys.exit(1)
+    # Gravando o log no arquivo `prefixcalc.log`
+    path = os.curdir
+    filepath = os.path.join(path, "prefixcalc.log")
+    timestamp = datetime.now().isoformat()
+    user = os.getenv("USER", "anonymous")
 
-validated_nums = []
-for num in nums:
-    # TODO: Repetição while + exceptions
-    if not num.replace(".", "").isdigit():
-        print(f"Número inválido {num}")
+    print(f"O resultado é {result}")
+
+    try:
+        with open(filepath, "a") as file_:
+            file_.write(f"{timestamp} - {user} " 
+                        f"- {operation}, {n1}, {n2} = {result}\n")
+
+    except PermissionError as e:
+        print(str(e))
         sys.exit()
-    if "." in num:
-        num = float(num)
-    else:
-        num = int(num)
-    validated_nums.append(num)
-  
-
-n1, n2 = validated_nums
-
-
-# TODO: Usar dict de funções
-if operation == "sum":
-    result = n1 + n2
-elif operation == "sub":
-    result = n1 - n2
-elif operation == "mult":
-    result = n1 * n2
-elif operation == "div":
-    result = n1 / n2
-
-# Gravando o log no arquivo `prefixcalc.log`
-path = os.curdir
-filepath = os.path.join(path, "prefixcalc.log")
-timestamp = datetime.now().isoformat()
-user = os.getenv("USER", "anonymous")
-
-print(f"O resultado é {result}")
-
-try:
-    with open(filepath, "a") as file_:
-        file_.write(f"{timestamp} - {user} - {operation}, {n1}, {n2} = {result}\n")
-
-except PermissionError as e:
-    print(str(e))
-    sys.exit()
     
-    
+    if input("Pressione ENTER para continuar ou qualquer tecla para sair:  "):
+        break
