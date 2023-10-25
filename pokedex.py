b265b0e65fd403b58af0cbfd3ca9b2e25c84a8bd
CarlosHNB <carloshnb.um@gmail.com>
2023-10-25 20:15:29 -0300

    Ideia geral do que a aplicação vai fazer

--- pokedex.py
+++ pokedex.py
@@ -15,8 +15,12 @@ url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}"
 request = requests.get(url)
 request_json = json.loads(request.content)
 
-print(f"O nome do pokémon é: {request_json['name']}")
+print(f"#ID: {request_json['id']}")
+print(f"Nome: {request_json['name']}")
 for i in range(0,2):
     ability_pokemon.append(request_json['abilities'][i]['ability']['name'])
 
-print(f"As habilidades do pokemon são: {ability_pokemon}")
+hab1, hab2 = ability_pokemon
+
+print(f"Habilidades: {hab1} e {hab2}")
+print(f"Tipo: {request_json['types'][0]['type']['name']}")
